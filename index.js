6f7faa5c441a4474953d304621e07cedc43a65a7
Mikal-E <melamin101@gmail.com>
2026-06-19 18:54:05 -0400

    Mounted parsing and static files, middleware. Set view engine.

--- index.js
+++ index.js
@@ -2,3 +2,26 @@
 Spent a good deal of time researching best practices for code and directories organization.
 Organized files based upon types and usage, which actually is similar to Lab 318.3.1 - Expanding a RESTful API */
 
+import express from "express"
+
+const app = express();
+const port = 3000;
+
+// Parsing Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// View Engine Set
+
+app.set("view engine", "ejs");
+
+// Static Files Middleware
+
+app.use("/public", express.static("public"));
+
+app.listen(port, () => {
+
+    console.log(`Server listening on port: ${port}.`);
+
+});
