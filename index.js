a7d69900514fc3cf369c9d80d3613da61970a468
Mikal-E <melamin101@gmail.com>
2026-06-19 17:58:02 -0400

    Installed packages/dependencies.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Requirements - Utilize reasonable code organization practices:
+/* Requirements - Utilize reasonable code organization practices, Weight 5%:
 Spent a good deal of time researching best practices for code and directories organization.
 Organized files based upon types and usage, which actually is similar to Lab 318.3.1 - Expanding a RESTful API */
 
