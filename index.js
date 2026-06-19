74bd45c147fef47c7dda2cef754aa7710ba829ef
Mikal-E <melamin101@gmail.com>
2026-06-19 16:07:22 -0400

    Spent a good deal of time researching best practices for code and directories organization.
    Organized files based upon types and usage, which actually is similar to Lab 318.3.1 - Expanding a RESTful API

---
+++ index.js
@@ -0,0 +1,4 @@
+/* Requirements - Utilize reasonable code organization practices:
+Spent a good deal of time researching best practices for code and directories organization.
+Organized files based upon types and usage, which actually is similar to Lab 318.3.1 - Expanding a RESTful API */
+
