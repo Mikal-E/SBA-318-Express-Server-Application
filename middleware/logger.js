be84f8c7efd5ecab32220de3dd7904bc3ec57be8
Mikal-E <melamin101@gmail.com>
2026-06-20 19:48:49 -0400

    Created custom middleware #2 - a validation middleware.

--- middleware/logger.js
+++ middleware/logger.js
@@ -5,14 +5,14 @@ Requirements - Create and use at least two pieces of custom middleware, Weight 5
 const logger = (req, res, next) => {
   const time = new Date();
 
-  console.log(
-    `-----
-${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
-  );
+  console.log(`----- ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`);
+  
   if (Object.keys(req.body).length > 0) {
+
     console.log("Containing the data:");
     console.log(`${JSON.stringify(req.body)}`);
   }
+  
   next();
 };
 
---
+++ middleware/validator.js
@@ -0,0 +1,22 @@
+/* Request Validation Middleware (Custom Middleware #2): Checks if Content-Type header is application/json on POST and PATCH requests
+Requirements - Create and use at least two pieces of custom middleware, Weight 5%.
+*/
+
+const validator = (req, res, next) => {
+
+    if (req.method === "POST" || req.method === "PATCH") {
+        
+        if (!req.headers["content-type"].includes("application/json")) {
+
+            return res.status(400).json({ error: "Content-Type must be application/json"});
+            
+        }
+
+    }
+
+    next();
+
+}
+
+export default validator;
+
