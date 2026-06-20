3a7d6de2795126bc0ac9da0658d9728f467c417c
Mikal-E <melamin101@gmail.com>
2026-06-20 19:22:04 -0400

    Created custom middleware in logger.js file.

---
+++ middleware/logger.js
@@ -0,0 +1,19 @@
+/* Logging Middleware (Custom Middleware #1)
+Requirements - Create and use at least two pieces of custom middleware, Weight 5%.
+*/
+
+const logger = (req, res, next) => {
+  const time = new Date();
+
+  console.log(
+    `-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+  );
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+};
+
+export default logger;
